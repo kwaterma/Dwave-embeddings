2b4abc592d63593362ca31bcc3f20f2909c2523b
kwaterma <77468998+kwaterma@users.noreply.github.com>
2021-03-19 12:06:41 -0400

    Add files via upload

---
+++ KnC_timed.py
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --time=00:15:00
+#SBATCH --job-name=KnC_timed
+#SBATCH --output=%x-%j.out
+
+from minorminer import find_embedding
+import networkx as nx
+import dwave_networkx as dnx
+import time
+
+for n in range(1,70):
+    startTime = time.time()
+    q = 1
+    g = (n-2)//4+1
+    nodes = 0
+    edges = n*(n-1)/2
+    chains = 0
+    c_edges = 0
+    lc = 0   #longest chain
+    Kn = find_embedding(nx.complete_graph(n), dnx.chimera_graph(g,g,4), random_seed=q)
+    for i in range(0,n):
+        nodes += len(Kn[i])
+        if len(Kn[i]) > 1:
+            chains += 1
+            edges += len(Kn[i])-1
+            c_edges += len(Kn[i])-1
+            if len(Kn[i]) > lc:
+                lc = len(Kn[i])
+    t = (time.time()-startTime)
+    print("n=",n,"cells=",g**2,"nodes=",nodes,"edges=",edges,"chains=",chains,"chain edges=",c_edges,"longest chain=",lc,"seed=",q,"time=",t)
+    print(Kn)
