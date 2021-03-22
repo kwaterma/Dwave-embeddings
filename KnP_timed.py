34fff5fd87e9f7f46488bc29854d2c1d01fa8e0f
kwaterma <77468998+kwaterma@users.noreply.github.com>
2021-03-22 14:18:08 -0400

    Add files via upload

---
+++ KnP_timed.py
@@ -0,0 +1,32 @@
+from minorminer import find_embedding
+import networkx as nx
+import dwave_networkx as dnx
+import time
+
+def pegasus1(g): # 4 pegasus graphs in a single layer (4 edges between cells)
+    G=dnx.chimera_graph(g,g,4)
+    for a in range(0,g*g*8-1,2):
+        G.add_edge(a,a+1)
+    return G
+
+for n in range(7,161):
+    startTime = time.time()
+    q = 1
+    g = (n+1)//4 #(works for pegasus1(g) for n>2
+    nodes = 0
+    edges = n*(n-1)/2
+    chains = 0
+    c_edges = 0
+    lc = 0   #longest chain
+    Kn = find_embedding(nx.complete_graph(n), dnx.pegasus_graph(g), random_seed=q)
+    for i in range(0,n):
+        nodes += len(Kn[i])
+        if len(Kn[i]) > 1:
+            chains += 1
+            edges += len(Kn[i])-1
+            c_edges += len(Kn[i])-1
+            if len(Kn[i]) > lc:
+                lc = len(Kn[i])
+        t = (time.time()-startTime)
+        print("n=",n,"cells=",g**2,"nodes=",nodes,"edges=",edges,"chains=",chains,"chain edges=",c_edges,"longest chain=",lc,"seed=",g,"time=",t)
+        print(Kn)
