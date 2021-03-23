11c6987baa44b744f9167b6eb612b0e46e92897c
kwaterma <77468998+kwaterma@users.noreply.github.com>
2021-03-22 23:27:49 -0400

    Update 0submit.sh

--- 0submit.sh
+++ 0submit.sh
@@ -4,5 +4,8 @@
 #SBATCH --output=%x-%j.out
 #SBATCH --error=ERROR-%j.out
 #SBATCH --account=def-nike-ab
+#SBATCH --mem=3900
+#SBATCH --mail-user=kathryn@pinkninja.net
+#SBATCH --mail-type=ALL
 
 python KnC_timed.py
