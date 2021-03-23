e166764896a819581a1c030c7d9f44e3936627f7
kwaterma <77468998+kwaterma@users.noreply.github.com>
2021-03-23 16:04:10 -0400

    Create KnP_repeat.py

---
+++ KnP_repeat.py
@@ -0,0 +1,27 @@
+from minorminer import find_embedding
+import networkx as nx
+import dwave_networkx as dnx
+import time
+
+for q in range(1,5000):
+    startTime = time.time()
+    n = 40
+    g = (n-3)//4
+    nodes = 0
+    edges = n*(n-1)/2
+    chains = 0
+    c_edges = 0
+    lc = 0   #longest chain
+    Kn = find_embedding(nx.complete_graph(n), dnx.pegasus_graph(g), random_seed=q)
+    for i in range(0,n):
+        nodes += len(Kn[i])
+        if len(Kn[i]) > 1:
+            chains += 1
+            edges += len(Kn[i])-1
+            c_edges += len(Kn[i])-1
+            if len(Kn[i]) > lc:
+                lc = len(Kn[i])
+    t = (time.time()-startTime)
+    print("n=",n,"cells=",g**2,"nodes=",nodes,"edges=",int(edges),"chains=",chains,"chain edges=",c_edges,"longest chain=",lc,"seed=",q,"time=",t)
+    print(Kn)
+print("done")
