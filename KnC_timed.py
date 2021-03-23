58ea1f55d58c3d3ae67f1943d2a78067e88215ae
kwaterma <77468998+kwaterma@users.noreply.github.com>
2021-03-22 22:02:19 -0400

    Update KnC_timed.py

--- KnC_timed.py
+++ KnC_timed.py
@@ -5,14 +5,14 @@ import time
 
 for n in range(3,161):
     startTime = time.time()
-    q = 1
+    s = 1
     g = (n-2)//4+1
     nodes = 0
     edges = n*(n-1)/2
     chains = 0
     c_edges = 0
     lc = 0   #longest chain
-    Kn = find_embedding(nx.complete_graph(n), dnx.chimera_graph(g,g,4), random_seed=q)
+    Kn = find_embedding(nx.complete_graph(n), dnx.chimera_graph(g,g,4), random_seed=s)
     for i in range(0,n):
         nodes += len(Kn[i])
         if len(Kn[i]) > 1:
@@ -22,5 +22,5 @@ for n in range(3,161):
             if len(Kn[i]) > lc:
                 lc = len(Kn[i])
     t = (time.time()-startTime)
-    print("n=",n,"cells=",g**2,"nodes=",nodes,"edges=",edges,"chains=",chains,"chain edges=",c_edges,"longest chain=",lc,"seed=",q,"time=",t)
+    print("n=",n,"cells=",g**2,"nodes=",nodes,"edges=",edges,"chains=",chains,"chain edges=",c_edges,"longest chain=",lc,"seed=",s,"time=",t)
     print(Kn)
