cbde9cd94ef6a64d50328219b4e441a267d0fd87
kwaterma <77468998+kwaterma@users.noreply.github.com>
2021-03-23 16:09:24 -0400

    Create 4submit.sh

---
+++ 4submit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH --time=71:00:00
+#SBATCH --job-name=KnP_repeat
+#SBATCH --output=%x-%j.out
+#SBATCH --error=ERROR-%j.out
+#SBATCH --account=def-nike-ab
+#SBATCH --mem=3900
+#SBATCH --mail-user=kathryn@pinkninja.net
+#SBATCH --mail-type=ALL
+
+module load python python/3.7.4
+source ~/ENV/bin/activate
+python KnP_repeat.py
