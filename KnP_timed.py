be051392a82fe442cc1c385bb8bc717f1c2fd2d9
kwaterma <77468998+kwaterma@users.noreply.github.com>
2021-03-22 23:35:37 -0400

    Update KnP_timed.py

--- KnP_timed.py
+++ KnP_timed.py
@@ -9,16 +9,16 @@ def pegasus1(g): # 4 pegasus graphs in a single layer (4 edges between cells)
         G.add_edge(a,a+1)
     return G
 
-for n in range(7,161):
+for n in range(120,201):
     startTime = time.time()
-    q = 1
+    s = 1
     g = (n+1)//4 #(works for pegasus1(g) for n>2
     nodes = 0
     edges = n*(n-1)/2
     chains = 0
     c_edges = 0
     lc = 0   #longest chain
-    Kn = find_embedding(nx.complete_graph(n), dnx.pegasus_graph(g), random_seed=q)
+    Kn = find_embedding(nx.complete_graph(n), dnx.pegasus_graph(g), random_seed=s)
     for i in range(0,n):
         nodes += len(Kn[i])
         if len(Kn[i]) > 1:
@@ -28,5 +28,6 @@ for n in range(7,161):
             if len(Kn[i]) > lc:
                 lc = len(Kn[i])
     t = (time.time()-startTime)
-    print("n=",n,"cells=",g**2,"nodes=",nodes,"edges=",edges,"chains=",chains,"chain edges=",c_edges,"longest chain=",lc,"seed=",q,"time=",t)
+    print("n=",n,"cells=",g**2,"nodes=",nodes,"edges=",edges,"chains=",chains,"chain edges=",c_edges,"longest chain=",lc,"seed=",s,"time=",t)
     print(Kn)
+print("done")
