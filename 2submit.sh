055669335a6fc78525640130396a65b858abb781
kwaterma <77468998+kwaterma@users.noreply.github.com>
2021-03-24 15:57:35 -0400

    Update 2submit.sh

--- 2submit.sh
+++ 2submit.sh
@@ -2,7 +2,7 @@
 #SBATCH --time=71:00:00
 #SBATCH --job-name=KnP1_timed
 #SBATCH --output=%x-%j.out
-#SBATCH --error=ERROR-%j.out
+#SBATCH --error=%x-%j.err
 #SBATCH --account=def-nike-ab
 #SBATCH --mem=3900
 #SBATCH --mail-user=kathryn@pinkninja.net
