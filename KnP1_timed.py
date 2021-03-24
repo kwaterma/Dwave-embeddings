762e6146fb87facefcc89651f92030db268c127a
kwaterma <77468998+kwaterma@users.noreply.github.com>
2021-03-24 19:28:16 -0400

    Update KnP1_timed.py

--- KnP1_timed.py
+++ KnP1_timed.py
@@ -9,7 +9,7 @@ def pegasus1(g): # 4 pegasus graphs in a single layer (4 edges between cells)
         G.add_edge(a,a+1)
     return G
 
-for n in range(200,501):
+for n in range(208,501):
     startTime = time.time()
     s = 1
     g = (n+1)//4+4 #+0 works for pegasus1(g) for n>2, +1 for n>157, +2 for n>178, +3 for n>190, +4 for n>192
