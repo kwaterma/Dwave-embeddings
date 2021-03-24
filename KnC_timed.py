0984eef6cdf7180c1c20b67113059aedcff7d1a7
kwaterma <77468998+kwaterma@users.noreply.github.com>
2021-03-24 00:15:18 -0400

    Update KnC_timed.py

--- KnC_timed.py
+++ KnC_timed.py
@@ -3,10 +3,10 @@ import networkx as nx
 import dwave_networkx as dnx
 import time
 
-for n in range(172,201):
+for n in range(184,201):
     startTime = time.time()
     s = 1
-    g = (n-2)//4+4 #+1 works for n>1, +2 for n>155, +3 for n>157, +4 for n>172
+    g = (n-2)//4+5 #+1 works for n>1, +2 for n>155, +3 for n>157, +4 for n>172, +5 for n>184
     nodes = 0
     edges = n*(n-1)/2
     chains = 0
