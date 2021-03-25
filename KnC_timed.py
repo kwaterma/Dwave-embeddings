d2f2bcaaf85e42547b325baea56b2519c0d47297
kwaterma <77468998+kwaterma@users.noreply.github.com>
2021-03-25 11:02:34 -0400

    Update KnC_timed.py

--- KnC_timed.py
+++ KnC_timed.py
@@ -3,15 +3,16 @@ import networkx as nx
 import dwave_networkx as dnx
 import time
 
-for n in range(220,501):
+for n in range(1,501):
     startTime = time.time()
     s = 1
-    g = (n-2)//4+7 #+1 works for n>1, +2 for n>155, +3 for n>157, +4 for n>172, +5 for n>184, +6 for >214, +7 for n>220,
+    g = (n-2)//4+1 #+1 works for n>1, +2 for n>155, +3 for n>157, +4 for n>172, +5 for n>184, +6 for >214, +7 for n>220,
     nodes = 0
     edges = n*(n-1)/2
     chains = 0
     c_edges = 0
     lc = 0   #longest chain
+    sc = 50 #shortest chain
     Kn = find_embedding(nx.complete_graph(n), dnx.chimera_graph(g,g,4), random_seed=s)
     for i in range(0,n):
         nodes += len(Kn[i])
@@ -21,7 +22,9 @@ for n in range(220,501):
             c_edges += len(Kn[i])-1
             if len(Kn[i]) > lc:
                 lc = len(Kn[i])
+            if len(Kn[i]) < sc:
+                sc = len(Kn[i])
     t = (time.time()-startTime)
-    print("n=",n,"cells=",g**2,"nodes=",nodes,"edges=",int(edges),"chains=",chains,"chain edges=",c_edges,"longest chain=",lc,"seed=",s,"time=",t)
+    print("n=",n,"cells=",g**2,"nodes=",nodes,"edges=",int(edges),"chains=",chains,"chain edges=",c_edges,"longest chain=",lc,"chain difference",int(lc-sc),"seed=",s,"time=",t)
     print(Kn)
 print("done")
