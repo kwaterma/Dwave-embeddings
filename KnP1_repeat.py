64f9bd412c1d5ddd33fcfdebbe7a46f7c9abd00b
kwaterma <77468998+kwaterma@users.noreply.github.com>
2021-03-26 17:22:03 -0400

    Update KnP1_repeat.py

--- KnP1_repeat.py
+++ KnP1_repeat.py
@@ -19,19 +19,22 @@ for s in range(1,200000):
     g = (n+1)//4 #works for pegasus1(g) for n>2
     nodes = 0
     edges = n*(n-1)/2
-    chains = 0
+    c = 0
     c_edges = 0
     lc = 0   #longest chain
+    sc = 100 #shortest chain
     Kn = find_embedding(nx.complete_graph(n), pegasus1(g), random_seed=s)
     for i in range(0,n):
         nodes += len(Kn[i])
+        if len(Kn[i]) < sc:
+            sc = len(Kn[i])
         if len(Kn[i]) > 1:
-            chains += 1
+            c += 1
             edges += len(Kn[i])-1
             c_edges += len(Kn[i])-1
             if len(Kn[i]) > lc:
                 lc = len(Kn[i])
     t = (time.time()-startTime)
-    print("n=",n,"cells=",g**2,"nodes=",nodes,"edges=",int(edges),"chains=",chains,"chain edges=",c_edges,"longest chain=",lc,"seed=",s,"time=",t)
+    print("n=",n,"cells=",g**2,"nodes=",nodes,"edges=",int(edges),"chains=",c,"chain edges=",c_edges,"longest chain=",lc,"chain difference=",(lc-sc),"seed=",s,"time=",t)
     print(Kn)
 print("done")
