ac74a7a03d925d1ebcbb249371d08ef989393b96
kwaterma <77468998+kwaterma@users.noreply.github.com>
2021-03-26 17:23:24 -0400

    Update KnP1_timed.py

--- KnP1_timed.py
+++ KnP1_timed.py
@@ -22,14 +22,14 @@ for n in range(237,501):
     Kn = find_embedding(nx.complete_graph(n), pegasus1(g), random_seed=s)
     for i in range(0,n):
         nodes += len(Kn[i])
+        if len(Kn[i]) < sc:
+            sc = len(Kn[i])
         if len(Kn[i]) > 1:
             c += 1
             edges += len(Kn[i])-1
             c_edges += len(Kn[i])-1
             if len(Kn[i]) > lc:
                 lc = len(Kn[i])
-            if len(Kn[i]) < sc:
-                sc = len(Kn[i])
     t = (time.time()-startTime)
     print("n=",n,"cells=",g**2,"nodes=",nodes,"edges=",int(edges),"chains=",c,"chain edges=",c_edges,"longest chain=",lc,"chain difference=",int(lc-sc),"seed=",s,"time=",t)
     print(Kn)
