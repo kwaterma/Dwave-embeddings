d3ef2fa1c528e6150406c2da9175d87d6711d7ba
kwaterma <77468998+kwaterma@users.noreply.github.com>
2021-03-26 17:25:34 -0400

    Update KnP_repeat.py

--- KnP_repeat.py
+++ KnP_repeat.py
@@ -8,24 +8,26 @@ n = int(sys.argv[1])
 if n < 7:
     raise Exception('no edges for Kn for n<7')
 
-for s in range(1,200000):
+for s in range(1,200001):
     startTime = time.time()
     g = (n+1)//4 #(works for pegasus_graph(g) for n>6
     nodes = 0
     edges = n*(n-1)/2
-    chains = 0
+    c = 0
     c_edges = 0
     lc = 0   #longest chain
     Kn = find_embedding(nx.complete_graph(n), dnx.pegasus_graph(g), random_seed=s)
     for i in range(0,n):
         nodes += len(Kn[i])
+        if len(Kn[i]) < sc:
+                sc = len(Kn[i])
         if len(Kn[i]) > 1:
-            chains += 1
+            c += 1
             edges += len(Kn[i])-1
             c_edges += len(Kn[i])-1
             if len(Kn[i]) > lc:
                 lc = len(Kn[i])
     t = (time.time()-startTime)
-    print("n=",n,"M=",g,"nodes=",nodes,"edges=",int(edges),"chains=",chains,"chain edges=",c_edges,"longest chain=",lc,"seed=",s,"time=",t)
+    print("n=",n,"M=",g,"nodes=",nodes,"edges=",int(edges),"chains=",c,"chain edges=",c_edges,"longest chain=",lc,"chain difference=",int(lc-sc),"seed=",s,"time=",t)
     print(Kn)
 print("done")
