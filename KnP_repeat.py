8762078610e5030e36807b8e0582752d6d25a05d
kwaterma <77468998+kwaterma@users.noreply.github.com>
2021-04-14 11:05:12 -0400

    Update KnP_repeat.py

--- KnP_repeat.py
+++ KnP_repeat.py
@@ -10,7 +10,7 @@ if n < 7:
 
 for s in range(1,200001):
     startTime = time.time()
-    g = (n+1)//4+1 #+1 for n>160
+    g = (n+1)//4+2 #+1 for n>160, +2 for n>170
     nodes = 0
     edges = n*(n-1)/2
     c = 0
