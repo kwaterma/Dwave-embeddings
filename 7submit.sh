b4c097c0e2b808a1a65b79e1cbb5b06ea7b04333
kwaterma <77468998+kwaterma@users.noreply.github.com>
2021-04-23 12:58:37 -0400

    Create 7submit.sh

---
+++ 7submit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH --time=71:00:00
+#SBATCH --job-name=KmnC_timed
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+#SBATCH --account=def-nike-ab
+#SBATCH --mem=3900
+#SBATCH --mail-user=kathryn@pinkninja.net
+#SBATCH --mail-type=ALL
+
+module load python python/3.7.4
+source ~/ENV/bin/activate
+python KmnC_timed.py
