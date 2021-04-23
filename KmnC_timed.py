52fd38f8f82d8e35a75ec1e2dbea7c1c5d11e368
kwaterma <77468998+kwaterma@users.noreply.github.com>
2021-04-23 13:14:26 -0400

    Update KmnC_timed.py

--- KmnC_timed.py
+++ KmnC_timed.py
@@ -8,25 +8,25 @@ for m in range(2,24):
         print(m,n,g)
         startTime = time.time()
         s = 1
-        g = int((((m//2)*(n//4))^^0.5)//2)
+        g = int((((m//2)*(n//4))**0.5)//2)
         nodes = 0
-        edges = n*(n-1)/2
+        edges = m*n
         c = 0
         c_edges = 0
         lc = 0   #longest chain
         sc = 100 #shortest chain
-        Kn = find_embedding(nx.complete_bipartite_graph(m,n), dnx.chimera_graph(g,g,4), random_seed=s)
-        for i in range(0,n):
-            nodes += len(Kn[i])
-            if len(Kn[i]) < sc:
-                sc = len(Kn[i])
-            if len(Kn[i]) > 1:
-                c += 1
-                edges += len(Kn[i])-1
-                c_edges += len(Kn[i])-1
-                if len(Kn[i]) > lc:
-                    lc = len(Kn[i])
+#        Kn = find_embedding(nx.complete_bipartite_graph(m,n), dnx.chimera_graph(g,g,4), random_seed=s)
+#        for i in range(0,m+n):
+#            nodes += len(Kn[i])
+#            if len(Kn[i]) < sc:
+#                sc = len(Kn[i])
+#            if len(Kn[i]) > 1:
+#                c += 1
+#                edges += len(Kn[i])-1
+#                c_edges += len(Kn[i])-1
+#                if len(Kn[i]) > lc:
+#                    lc = len(Kn[i])
         t = (time.time()-startTime)
-        print("m=",m,"n=",n,"cells=",g**2,"nodes=",nodes,"edges=",int(edges),"chains=",c,"chain edges=",c_edges,"longest chain=",lc,"chain difference=",int(lc-sc),"seed=",s,"time=",t)
-        print(Kn)
+#        print("m=",m,"n=",n,"cells=",g**2,"nodes=",nodes,"edges=",int(edges),"chains=",c,"chain edges=",c_edges,"longest chain=",lc,"chain difference=",int(lc-sc),"seed=",s,"time=",t)
+#        print(Kn)
 print("done")
