40930316400efbf6a7a5c1c238bd8b402ae9f7da
kwaterma <77468998+kwaterma@users.noreply.github.com>
2021-04-30 15:17:38 -0400

    Update KnC_repeat.py

--- KnC_repeat.py
+++ KnC_repeat.py
@@ -1,24 +1,26 @@
+#will calculate the embedding of a Kn graph onto Chimera for a single value of n and every value of s
+#can be used to find the best possible version of the embedding if run to a high enough seed
+
 from minorminer import find_embedding
 import networkx as nx
 import dwave_networkx as dnx
 import time
 import sys
 
-n = int(sys.argv[1])
-if n < 2:
-    raise Exception('no edges for Kn for n<2')
-
-for s in range(1,200001):
+n = int(sys.argv[1]) #get n from the command line, works for n=2 to n=150 (untested, maybe higher), adding extra cells step needs to be refined
+for s in range(1,200001): #seed
     startTime = time.time()
-    g = (n-2)//4+2 #+1 for n>2, +2 for n>140, +3 for n>160, +4 for n>170, +5 for n>175, +6 for n>180, +7 for n>185
+    g = (n-2)//4+1 #graph size, +1 works for n<135, after that extra cells are added
+    if n > 135: #somewhat corrects extra cells needed for larger calculations
+        g += (n-125)//10
     nodes = 0
-    edges = n*(n-1)/2
-    c = 0
-    c_edges = 0
-    lc = 0   #longest chain
-    sc = 100 #shortest chain
+    edges = n*(n-1)/2 #calculate how many regular edges are in the Kn graph, before chains are added in the embedding
+    c = 0 #chains
+    c_edges = 0 #chain edges
+    lc = 1 #longest chain
+    sc = 1000 #shortest chain
     Kn = find_embedding(nx.complete_graph(n), dnx.chimera_graph(g,g,4), random_seed=s)
-    for i in range(0,n):
+    for i in range(0,n): #go through each node in the source graph to update totals. may be better done as collecting lists of each values and adding totals after, allowing averages and offsets to be found as well
         nodes += len(Kn[i])
         if len(Kn[i]) < sc:
             sc = len(Kn[i])
@@ -27,7 +29,7 @@ for s in range(1,200001):
             edges += len(Kn[i])-1
             c_edges += len(Kn[i])-1
             if len(Kn[i]) > lc:
-                lc = len(Kn[i])
+                lc = len(Kn[i]) #lc counts how many nodes are in the longest chain, not how many chain edges there are
     t = (time.time()-startTime)
     print("n=",n,"cells=",g**2,"nodes=",nodes,"edges=",int(edges),"chains=",c,"chain edges=",c_edges,"longest chain=",lc,"chain differnce",int(lc-sc),"seed=",s,"time=",t)
     print(Kn)
