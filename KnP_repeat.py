d87a5acaf4412791e655b972bf0dd5fed5af6380
kwaterma <77468998+kwaterma@users.noreply.github.com>
2021-04-30 15:18:29 -0400

    Update KnP_repeat.py

--- KnP_repeat.py
+++ KnP_repeat.py
@@ -4,21 +4,18 @@ import dwave_networkx as dnx
 import time
 import sys
 
-n = int(sys.argv[1])
-if n < 7:
-    raise Exception('no edges for Kn for n<7')
-
-for s in range(1,200001):
+n = int(sys.argv[1]) #works for n=3 to an untested upper bound, adding extra cells step needs to be added
+for s in range(1,200001): #seed
     startTime = time.time()
-    g = (n+1)//4+4 #+1 for n>160, +2 for n>170, +3 for n>175, +4 for n>180
+    g = (n+22)//12 #graph size, untested, after some point will need extra cells need to be added
     nodes = 0
-    edges = n*(n-1)/2
-    c = 0
-    c_edges = 0
-    lc = 0   #longest chain
-    sc = 100 #shortest chain
+    edges = n*(n-1)/2 #calculate how many regular edges are in the Kn graph, before chains are added in the embedding
+    c = 0 #chains
+    c_edges = 0 #chain edges
+    lc = 1 #longest chain
+    sc = 1000 #shortest chain
     Kn = find_embedding(nx.complete_graph(n), dnx.pegasus_graph(g), random_seed=s)
-    for i in range(0,n):
+    for i in range(0,n): #go through each node in the source graph to update totals. may be better done as collecting lists of each values and adding totals after, allowing averages and offsets to be found as well
         nodes += len(Kn[i])
         if len(Kn[i]) < sc:
                 sc = len(Kn[i])
@@ -27,7 +24,7 @@ for s in range(1,200001):
             edges += len(Kn[i])-1
             c_edges += len(Kn[i])-1
             if len(Kn[i]) > lc:
-                lc = len(Kn[i])
+                lc = len(Kn[i]) #lc counts how many nodes are in the longest chain, not how many chain edges there are
     t = (time.time()-startTime)
     print("n=",n,"M=",g,"nodes=",nodes,"edges=",int(edges),"chains=",c,"chain edges=",c_edges,"longest chain=",lc,"chain difference=",int(lc-sc),"seed=",s,"time=",t)
     print(Kn)
